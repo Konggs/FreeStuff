81486045650138054ca16742c48bd956ed54b241
Konggs <bao123.v5@gmail.com>
2025-09-19 21:47:03 +0700

    Update Hop.lua

--- Hop.lua
+++ Hop.lua
@@ -1,11 +1,8 @@
 local HttpService = game:GetService("HttpService")
 local TeleportService = game:GetService("TeleportService")
-local Player = game.Players.LocalPlayer
-
 local AllIDs, foundAnything = {}, ""
 local actualHour = os.date("!*t").hour
 local currentServers, serverIndex = {}, 1
-
 pcall(function()
     AllIDs = HttpService:JSONDecode(readfile("NotSameServers.json"))
 end)
@@ -15,14 +12,12 @@ if type(AllIDs) ~= "table" or AllIDs[1] ~= actualHour then
 else
     print("[DEBUG] Loaded NotSameServers for hour:", actualHour)
 end
-
 pcall(function()
     currentServers = HttpService:JSONDecode(readfile("ServerCache.json"))
 end)
 if type(currentServers) ~= "table" then
     currentServers = {}
 end
-
 local function GetServers(cursor)
     local url = ("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Asc&limit=100"):format(game.PlaceId)
     if cursor and cursor ~= "" then url ..= "&cursor=" .. cursor end
@@ -46,13 +41,11 @@ local function GetServers(cursor)
         pcall(function() writefile("ServerCache.json", HttpService:JSONEncode(currentServers)) end)
     end
 end
-
 local function EnsureServerCache()
     if #currentServers - serverIndex < 50 then
         GetServers(foundAnything)
     end
 end
-
 local function TPReturner()
     EnsureServerCache()
     local v = currentServers[serverIndex]
@@ -68,11 +61,7 @@ local function TPReturner()
     end
     table.insert(AllIDs, id)
     pcall(function() writefile("NotSameServers.json", HttpService:JSONEncode(AllIDs)) end)
-    TeleportService:TeleportToPlaceInstance(game.PlaceId, id, Player)
+    TeleportService:TeleportToPlaceInstance(game.PlaceId, id, game.Players.LocalPlayer)
 end
-
-TeleportService.TeleportInitFailed:Connect(function()
-    task.defer(TPReturner)
-end)
-
+TeleportService.TeleportInitFailed:Connect(function() task.defer(TPReturner) end)
 task.defer(TPReturner)
